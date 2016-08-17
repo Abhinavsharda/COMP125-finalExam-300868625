5f7b14d78b7afb144a8f24b46f9f8a95ca17bc1f
asharda1 <300868625@cencol.ca>
2016-08-17 14:18:20 -0400

    final commit

--- Scripts/app.js
+++ Scripts/app.js
@@ -3,6 +3,14 @@ var core;
     "use strict";
     var canvas;
     var stage;
+    
+    var dice = new createjs.Bitmap("Assets/images/1.png");
+    var dice = new createjs.Bitmap("Assets/images/2.png");
+    var dice = new createjs.Bitmap("Assets/images/3.png");
+    var dice = new createjs.Bitmap("Assets/images/4.png");
+    var dice = new createjs.Bitmap("Assets/images/5.png");
+    var dice = new createjs.Bitmap("Assets/images/6.png");
+
     // app entry function
     function init() {
         canvas = document.getElementById("canvas");
@@ -11,11 +19,32 @@ var core;
         createjs.Ticker.framerate = 60;
         createjs.Ticker.on("tick", gameLoop);
         main();
+  
     }
     function gameLoop() {
         stage.update();
     }
+    function rollButton_clicked() {
+      
+    }
     function main() {
+        var dice1 = new createjs.Bitmap("Assets/images/blank.png");
+        dice1.x = 50;
+        dice1.y = 50;
+        stage.addChild(dice1);
+       
+       
+        var dice2 = new createjs.Bitmap("Assets/images/blank.png");
+        dice2.x=400;
+        dice2.y=50;
+        stage.addChild(dice2);
+       
+       
+        var rollButton = new createjs.Bitmap(" Assets/images/rollButton.png");
+        rollButton.x = 250;
+        rollButton.y = 400;
+        stage.addChild(rollButton);
+        rollButton.addEventListener("click", rollButton_clicked);
     }
     window.addEventListener("load", init);
 })(core || (core = {}));
